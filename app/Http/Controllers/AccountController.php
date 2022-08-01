e4eb83bec4559b8c38d022b9908a3ad785bfd62a
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2022-08-01 14:42:54 -0500

    update alternate email

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Helpers\CurlCobain;
 use App\Helpers\LDAPH\EasyLDAP;
+use App\Http\Requests\VerifyTokenRequest;
 use App\Models\PasswordChangeRequest;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AccountController extends Controller
 {
@@ -36,30 +38,39 @@ class AccountController extends Controller
         $answerAsObject = json_decode($answer, true, 512, JSON_THROW_ON_ERROR);
 
         if (isset($answerAsObject['error'])) {
-            return response()->json(['message' => $answerAsObject['error']], 404);
+            return response()->json(['message' => 'Los datos ingresados no coinciden con nuestro registro. Verifica la información suministrada en el formulario'], 404);
         }
-
-        return response()->json(['message' => 'Estimado usuario, tu correo electrónico Unibagué es: ' . $answerAsObject[0]['cod_usuario']]);
+        $user = explode('@', $answerAsObject[0]['cod_usuario'])[0];
+        return response()->json(['message' => 'Tu usuario Unibagué es: ' . $user]);
     }
 
 
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \JsonException
+     */
     public function changeAlternateEmail(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required',
-            'alternateEmail' => 'required',
-            'confirmAlternateEmail' => 'required|same:alternateEmail',
-        ]);
+        try {
+            $this->validate($request, [
+                'user' => 'required',
+                'role' => 'required',
+                'password' => 'required',
+                'alternateEmail' => 'required',
+                'confirmAlternateEmail' => 'required|same:alternateEmail',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['message' => 'Por favor, ingresa todos los datos del formulario'], 404);
+        }
 
-
+        $email = $request->input('user') . ($request->input('role') === 0 ? '@estudiantesunibague.edu.co' : '@unibague.edu.co');
         $curl = new CurlCobain('https://academia.unibague.edu.co/atlante/actualiza_alterno.php');
         $curl->setQueryParamsAsArray([
             'consulta' => 'Consultar',
-            'account' => $request->input('email'),
+            'account' => $email,
             'password' => $request->input('password'),
             'alterno' => $request->input('alternateEmail'),
-            'token' => md5($request->input('email')) . "YHyd?B'r8R7ejTRN"
+            'token' => md5($email) . "YHyd?B'r8R7ejTRN"
         ]);
 
         $answer = $curl->makeRequest();
@@ -78,7 +89,7 @@ class AccountController extends Controller
      * @throws \JsonException
      * @throws \Exception
      */
-    public function recoverPassword(Request $request)
+    public function recoverPassword(Request $request): \Illuminate\Http\JsonResponse
     {
         $this->validate($request, [
             'email' => 'required|email',
@@ -123,49 +134,85 @@ class AccountController extends Controller
         return response()->json(['message' => 'Hemos enviado un enlace de recuperación de contraseña a tu correo alterno registrado: ' . "$user@$domain"]);
     }
 
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function verifyToken(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        $changePasswordRequest = PasswordChangeRequest::where('token', '=', $request->input('token'))->latest()->first();
+        if (!$changePasswordRequest) {
+            return response('', 404);
+        }
+        if ($changePasswordRequest->isActive === 1) {
+            return response('', 200);
+        }
+        return response('', 404);
+
+    }
+
     public function changePassword(Request $request)
     {
         $this->validate($request, [
-            'user' => 'required|string',
-            'password' => 'required',
+            'user' => 'required_without:token|string',
+            'password' => 'required_without:token',
             'newPassword' => 'required',
             'confirmNewPassword' => 'required|same:newPassword',
             'role' => 'required|numeric'
         ]);
-
+        //prepare LDAP and parse request inputs
         $easyLDAP = new EasyLDAP(false);
 
-        //Check if the credentials match
-        $user = $request->input('user');
-        $password = $request->input('password');
         $newPassword = $request->input('newPassword');
         $role = $request->input('role');
-        try {
-            $easyLDAP->authenticate($user, $password, $role);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Lo sentimos, los datos que ingresaste no coinciden con nuestros registros', 403]);
+
+        if ($request->input('token') === null) {
+            $user = $request->input('user');
+            $password = $request->input('password');
+
+            //Check if the credentials match
+            try {
+                $easyLDAP->authenticate($user, $password, $role);
+            } catch (\Exception $e) {
+                return response()->json(['message' => 'Lo sentimos, los datos que ingresaste no coinciden con nuestros registros'], 403);
+            }
+
+        } //The token is not null, verifies if exist and is valid.
+        else {
+            $token = $request->input('token');
+            //Find it
+            $passwordChangeRequest = PasswordChangeRequest::where('token', '=', $token)->first();
+            //not found
+            if (!$passwordChangeRequest) {
+                return response()->json(['message' => 'El código que ingresaste no es válido'], 404);
+            }
+            //The code is not active
+            if ($passwordChangeRequest->isActive === 0) {
+                return response()->json(['message' => 'El código que ingresaste ya ha expirado o ha sido usado'], 400);
+            }
+            //The code is active, proceed with password change
+            $user = explode('@', $passwordChangeRequest->email)[0];
+            $passwordChangeRequest->isActive = 0;
+            $passwordChangeRequest->save();
         }
 
         //authenticate as admin and change the password
         $easyLDAP->authenticateAsAdmin();
 
-        $filter = [
-            'uid' => $user,
-        ];
+        $filter = ['uid' => $user,];
 
         $result = $easyLDAP->getFirst($filter, $easyLDAP->roles[$role]);
 
-        $modifiedAttributes = [
-            'userPassword' => $easyLDAP::generateMD5Password($newPassword),
-        ];
+        $modifiedAttributes = ['userPassword' => $easyLDAP::generateMD5Password($newPassword),];
 
         $modify = $easyLDAP->modify($result['dn'], $modifiedAttributes);
         if ($modify === true) {
             return response()->json(['message' => 'Tu contraseña ha sido cambiada exitosamente'], 200);
         }
-
         return response()->json(['message' => 'Ha ocurrido un error interno, por favor intentalo mas tarde'], 500);
-
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ $router->get('/test', function () use ($router) {
     \Illuminate\Support\Facades\Mail::to('juan.ospina@unibague.edu.co')->send(new \App\Mail\RecoverPassword());
 });
 
+//Change password router
 $router->post('/changePassword', 'AccountController@changePassword');
+$router->post('/verifyToken', 'AccountController@verifyToken');
+
 $router->post('/rememberEmail', 'AccountController@rememberEmail');
 $router->post('/recoverPassword', 'AccountController@recoverPassword');
 $router->post('/changeAlternateEmail', 'AccountController@changeAlternateEmail');
